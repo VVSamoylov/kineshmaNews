4af76d6f396caa78d842e3ae2db21bd1d0a85481
MaK1aren <makarsmirnov2@gmail.com>
2022-04-02 15:53:28 +0300

    урок 2 апреля

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    article: [{
+      heading: 'Статья первая',
+      content: 'контент 1',
+      autor: 'Бублико',
+      category: 'Общество',
+      description: 'Пожирание бублико',
+      dateArticle: ' '
+    },
+    {
+      heading: 'Статья вторая',
+      content: 'контент 2',
+      autor: 'Колбаскин',
+      category: 'Общество',
+      description: 'Пожирание колбасы',
+      dateArticle: ' '
+    }
+
+    ]
   },
   mutations: {
+    addArt(state, playload){
+      state.article.push(playload)
+    }
   },
   actions: {
+  },
+  getters:{
+      getArticle: state => {
+        return state.article
+      }
   },
   modules: {
   }
--- src/views/news/index.vue
+++ src/views/news/index.vue
@@ -1,6 +1,8 @@
 <template>
 <newsmenu />
 index
+
+{{getArt}}
 </template>
 
 <script>
@@ -9,6 +11,11 @@ export default{
     name: 'index',
     components:{
         newsmenu
+    },
+    computed:{
+        getArt(){
+            return this.$store.getters.getArticle
+        }
     }
 }
 </script>
